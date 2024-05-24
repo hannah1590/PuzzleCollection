928f41f3516c79b25bb88b1172ce6035d7d3ba1e
hannah1590 <hannah.fasco@mymail.champlain.edu>
2024-05-24 19:49:21 -0400

    reorganized some code and changed fillGrid to fillLine
    Only works half the time due to stack overflow with the recursive function. Still need to figure out a better way to randomize the grid

--- PuzzleCollection/GridManager.cpp
+++ PuzzleCollection/GridManager.cpp
@@ -1,18 +1,17 @@
 #include "GridManager.h";
-
-GridManager::GridManager(int size, int subX, int subY)
+GridManager::GridManager(int size, int boxX, int boxY)
 {
-    mSize = size;
-    mSubSquareX = subX;
-    mSubSquareY = subY;
+    srand(time(NULL));
 
-    for (int i = 1; i <= mSize; i++)
-    {
-        nums.push_back(i);
-    }
+    mSize = size;
+    mBoxSizeX = boxX;
+    mBoxSizeY = boxY;
 
     initGrid();
-    fillGrid();
+    for (int i = 0; i < mSize; i++)
+    {
+        fillLine(0, mSize - 1, i);
+    }
     printGrid();
 }
 
@@ -24,12 +23,30 @@ void GridManager::initGrid()
 {
     for (int i = 0; i < mSize; i++)
     {
+        nums.push_back(i + 1); // Fills the nums vector with all available number options
         std::vector<int> temp;
         for (int j = 0; j < mSize; j++)
         {
             temp.push_back(0);
         }
         grid.push_back(temp);
+    }
+}
+
+// Works half the time, but will often throw a stack overflow error; figure out a way to optimize it with less brute force and more reliabillity
+void GridManager::fillLine(int start, int end, int row)
+{
+    for (int i = start; i <= end; i++)
+    {
+        grid[i][row] = getAvailableNum(i, row);
+        if (grid[i][row] == 0 && i != 0)
+            fillLine(0, i, row);
+        else if (grid[i][row] == 0 && i == 0)
+        {
+            fillLine(0, mSize - 1, row - 1);
+            fillLine(0, i, row);
+        }
+            
     }
 }
 
@@ -45,78 +62,94 @@ void GridManager::initGrid()
 (0,2) (1,2) (2,2) | (3,2) (4,2) (5,2) | (6,2) (7,2) (8,2)
 (0,1) (1,1) (2,1) | (3,1) (4,1) (5,1) | (6,1) (7,1) (8,1)
 (0,0) (1,0) (2,0) | (3,0) (4,0) (5,0) | (6,0) (7,0) (8,0)
+
+(0,5) (1,5) (2,5) | (3,5) (4,5) (5,5) 
+(0,4) (1,4) (2,4) | (3,4) (4,4) (5,4) 
+-------------------------------------
+(0,3) (1,3) (2,3) | (3,3) (4,3) (5,3)
+(0,2) (1,2) (2,2) | (3,2) (4,2) (5,2)
+-------------------------------------
+(0,1) (1,1) (2,1) | (3,1) (4,1) (5,1)
+(0,0) (1,0) (2,0) | (3,0) (4,0) (5,0)
 */
 
 // Currently for Sudoku
 // Doesn't work yet, can't completely randomize a sudoku problem as the computer will get stuck eventually
 void GridManager::fillGrid()
-{          
-    srand(time(NULL));
+{
     for (int y = 0; y < mSize; y++)
     {
         for (int x = 0; x < mSize; x++)
         {
-            //printGrid();
-            int temp = 1;
-            std::vector<int> numAvailable = nums;
-            // Check columns for repeating numbers
-            while (x - temp >= 0)
-            {
-                // Remove the element using erase function and iterators 
-                auto it = std::find(numAvailable.begin(), numAvailable.end(),
-                    grid[x - temp][y]);
-
-                // If element is found found, erase it 
-                if (it != numAvailable.end()) {
-                    numAvailable.erase(it);
-                }
-                temp++;
-            }
-           
-            // Check rows for repeating numbers
-            temp = 1;
-            while (y - temp >= 0)
-            {
-                // Remove the element using erase function and iterators 
-                auto it = std::find(numAvailable.begin(), numAvailable.end(),
-                    grid[x][y - temp]);
-
-                // If element is found found, erase it 
-                if (it != numAvailable.end()) {
-                    numAvailable.erase(it);
-                }
-                temp++;
-            }
-            // Checks subsquare for repeats
-            for (int j = x % mSubSquareX; j >= 0; j--) // finds which column in subsquare x is at and goes left until it hits next subsquare/end for row y
-            {
-                // Remove the element using erase function and iterators 
-                auto it = std::find(numAvailable.begin(), numAvailable.end(),
-                    grid[x - j][y]);
-
-                // If element is found found, erase it 
-                if (it != numAvailable.end()) {
-                    numAvailable.erase(it);
-                }
-            }
-            for (int i = y % mSubSquareY; i > 0; i--) // starts at the beginning row of the current subsquare and goes up to the current row
-            {
-                for (int j = x - (x % mSubSquareX); j <= (x - (x % mSubSquareX)) + (mSubSquareX - 1); j++) // go through each column in the rows beneath the current row
-                {
-                    // Remove the element using erase function and iterators 
-                    auto it = std::find(numAvailable.begin(), numAvailable.end(),
-                        grid[j][y - i]);
-
-                    // If element is found found, erase it 
-                    if (it != numAvailable.end()) {
-                        numAvailable.erase(it);
-                    }
-                }
-            }
-            if(numAvailable.size() != 0)
-                grid[x][y] = numAvailable[rand() % numAvailable.size()];
+            grid[x][y] = getAvailableNum(x, y);
         }
     }
+}
+
+// Gets a randomly generated number that doesn't yet appear in the row/column/box; if one cannot be found returns 0
+int GridManager::getAvailableNum(int x, int y)
+{
+    std::vector<int> numAvailable = nums;
+
+    // Check columns for repeating numbers
+    int temp = 1;
+    while (x - temp >= 0)
+    {
+        // Finds location of the number
+        auto it = std::find(numAvailable.begin(), numAvailable.end(),
+            grid[x - temp][y]);
+
+        // Remove number from available
+        if (it != numAvailable.end()) {
+            numAvailable.erase(it);
+        }
+        temp++;
+    }
+
+    // Check rows for repeating numbers
+    temp = 1;
+    while (y - temp >= 0)
+    {
+        // Finds location of the number
+        auto it = std::find(numAvailable.begin(), numAvailable.end(),
+            grid[x][y - temp]);
+
+        // Remove number from available
+        if (it != numAvailable.end()) {
+            numAvailable.erase(it);
+        }
+        temp++;
+    }
+    // Checks box for repeats
+    for (int j = x % mBoxSizeX; j >= 0; j--) // Finds which column in box x is at and goes left until it hits next box/end for row y
+    {
+        // Finds location of the number
+        auto it = std::find(numAvailable.begin(), numAvailable.end(),
+            grid[x - j][y]);
+
+        // Remove number from available
+        if (it != numAvailable.end()) {
+            numAvailable.erase(it);
+        }
+    }
+    for (int i = y % mBoxSizeY; i > 0; i--) // Starts at the beginning row of the current box and goes up to the current row
+    {
+        for (int j = x - (x % mBoxSizeX); j <= (x - (x % mBoxSizeX)) + (mBoxSizeX - 1); j++) // Go through each column in the rows beneath the current row
+        {
+            // Finds location of the number
+            auto it = std::find(numAvailable.begin(), numAvailable.end(),
+                grid[j][y - i]);
+
+            // Remove number from available
+            if (it != numAvailable.end()) {
+                numAvailable.erase(it);
+            }
+        }
+    }
+    if (numAvailable.size() != 0)
+        return numAvailable[rand() % numAvailable.size()];
+    else
+        return 0;
 }
 
 void GridManager::printGrid()
@@ -126,13 +159,13 @@ void GridManager::printGrid()
         for (int j = 0; j < mSize; j++)
         {
             std::cout << grid[j][i] << " ";
-            if (j % mSubSquareX == mSubSquareX - 1)
+            if (j % mBoxSizeX == mBoxSizeX - 1)
             {
                 std::cout << "|";
             }
         }
         std::cout << std::endl;
-        if (i % mSubSquareY == mSubSquareY - 1)
+        if (i % mBoxSizeY == mBoxSizeY - 1)
         {
             std::cout << "----------------------------------" << std::endl;
         }
--- PuzzleCollection/GridManager.h
+++ PuzzleCollection/GridManager.h
@@ -5,14 +5,16 @@
 class GridManager : public Trackable
 {
 public:
-	GridManager(int size, int subX, int subY);
+	GridManager(int size, int boxX, int boxY);
 	~GridManager();
 
 	void initGrid();
 	void fillGrid();
 	void printGrid();
+	void fillLine(int start, int end, int row);
+	int getAvailableNum(int x, int y);
 private:
-	int mSize, mSubSquareX, mSubSquareY; // column and row size is always equal while the sub square sizes can be different but must multiply together to equal size
+	int mSize, mBoxSizeX, mBoxSizeY; // column and row size is always equal while the box sizes can be different but must multiply together to equal size
 	std::vector<int> nums;
 	std::vector<std::vector<int>> grid;
 };
